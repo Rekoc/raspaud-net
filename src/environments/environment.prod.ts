d16af6674ea986929778500eb4ed5aafd16393b0
Rekoc <rekoc13@gmail.com>
2020-03-01 20:08:14 +1000

    Update config file for production output.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: "AIzaSyA9CESTR_0kR05cPw0A9dIyIf8aaYU51bU",
+  authDomain: "raspaud-42a95.firebaseapp.com",
+  databaseURL: "https://raspaud-42a95.firebaseio.com",
+  projectId: "raspaud-42a95",
+  storageBucket: "raspaud-42a95.appspot.com",
+  messagingSenderId: "309460101440",
+  appId: "1:309460101440:web:6e0c540947df55f94d32a5",
+  measurementId: "G-LKZWTH5XZQ"
 };
